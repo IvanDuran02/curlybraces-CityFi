c4b33f9d49be345c04445b9c58733ba84ebf7859
RiveraJ94 <josephrivera1394@gmail.com>
2022-10-08 18:25:52 -0400

    Add photo metadata info

---
+++ pages/photoinfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const photoinfo = () => {
+  
+  return (
+    <div className="flex flex-col justify-start items-center">
+
+    <div className="py-2 px-2">
+      <img className="bg-contain w-38 rounded-md" src="https://assets.firestonecompleteautocare.com/content/dam/bsro-sites/fcac/blog/images/2019/03/highway-with-potholes-and-snow.jpeg" alt="pot hole"/>
+    </div>
+
+    <div>
+        <h1 className="underline font-bold text-2xl">Your photo information</h1>
+    </div>
+
+    <div>
+        <ul>
+            <li><span className="font-bold">Date:</span> October 9, 2022</li>
+            <li><span className="font-bold">Location:</span> 223456., 234234.00</li>
+            <li><span className="font-bold">Address:</span> 6708 Southwest 78th Hollywood,FL</li>
+        </ul>
+    </div>
+
+    <
+
+
+
+    </div>
+  );
+};
+
+export default photoinfo;
+
+
+  
+
+
+
