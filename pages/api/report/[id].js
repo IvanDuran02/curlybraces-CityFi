f8d6f6127c730571db724a9a1ae124e1eafa4cac
Lia Moua <moua0061@yahoo.com>
2022-10-09 06:23:30 -0400

    fixed all CRUD functionality. Currently working on PUT in report

--- pages/api/report/[id].js
+++ pages/api/report/[id].js
@@ -2,6 +2,7 @@ import prisma from '../../../prisma/client'
 
 //GET api/report/[id]
 //DELETE api/report/[id]
+//UPDATE api/report/[id]
 export default async function handler(req, res) {
         const { id } = req.query
     
@@ -14,14 +15,37 @@ export default async function handler(req, res) {
                 })
                 res.status(200).json(result)
             } else if(req.method === 'DELETE'){
-                const result = await prisma.report.delete({
+                const deletedReport = await prisma.report.delete({
                     where: {
                         id: Number(id)
                     }
                 })
-                res.status(202).json(`report id #${result.id} has been successfully deleted`)
+                res.status(202).json(`report id #${deletedReport.id} has been successfully deleted`)
+            } else if(req.method === 'PUT'){
+                const updatedReport = await prisma.report.updateMany({
+                    //update
+                    where: {
+                        id: {
+                            contains: Number(id)
+                        },
+                    },
+                    data: {
+                        status: updatedReport.status,
+                        latitude: updatedReport.latitude,
+                        longitude: updatedReport.longitude,
+                        category: updatedReport.category,
+                        subCategory: updatedReport.category,
+                        description: updatedReport.description
+                    }
+                })
+                console.log(updatedReport.id)
+                // res.status(202).json({
+                //     updatedReport,
+                //     message: `report # ${updatedReport.id} has been updated!`
+                // })
             }
         } catch (err){
-            res.status(405).json({err})
+            console.log(err)
+            //res.status(405).json({err})
         }
 }
